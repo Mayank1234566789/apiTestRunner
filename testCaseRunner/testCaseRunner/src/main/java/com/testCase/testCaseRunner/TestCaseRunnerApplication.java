aa594141e8b03aa6da8b9458382dec1cd7d8c012
Mayank1234566789 <mayankwork09@gmail.com>
2024-10-08 21:11:03 +0530

    Add files via upload

---
+++ testCaseRunner/testCaseRunner/src/main/java/com/testCase/testCaseRunner/TestCaseRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.testCase.testCaseRunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCaseRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCaseRunnerApplication.class, args);
+	}
+
+}
---
+++ testCaseRunner/testCaseRunner/src/test/java/com/testCase/testCaseRunner/TestCaseRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testCase.testCaseRunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCaseRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
